6ec940e7b70d9ea8db7d3a6faa6c4acc68b7ab50
xheartx <546593797@qq.com>
2017-06-09 22:59:09 +0800

    0

--- models/db.js
+++ models/db.js
@@ -2,11 +2,12 @@
  * @Author: X_Heart
  * @Date: 2017-06-09 10:33:51
  * @Last Modified by: X_Heart
- * @Last Modified time: 2017-06-09 11:12:13
+ * @Last Modified time: 2017-06-09 22:58:31
  * @description: mongoose数据库
  */
 const mongoose = require('mongoose')
-const db = mongoose.createConnection('mongodb://localhost/booksystem')
+const uristring = process.env.MONGODB_URI || 'mongodb://localhost/booksystem';
+const db = mongoose.createConnection(uristring)
 
 db.once('open', (callback) => {
   console.log('数据库连接成功')
