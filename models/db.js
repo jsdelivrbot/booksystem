65b61ee9357b4cc3f5b2d4efa015ee6acdc37344
xheartx <546593797@qq.com>
2017-06-10 00:12:32 +0800

    0

--- models/db.js
+++ models/db.js
@@ -2,11 +2,11 @@
  * @Author: X_Heart
  * @Date: 2017-06-09 10:33:51
  * @Last Modified by: X_Heart
- * @Last Modified time: 2017-06-10 00:07:34
+ * @Last Modified time: 2017-06-10 00:12:14
  * @description: mongoose数据库
  */
 const mongoose = require('mongoose')
-const uristring = 'mongodb://<dbuser>:<dbpassword>@ds113702.mlab.com:13702/xheart'
+const uristring = 'mongodb://x_heart:546593797@ds113702.mlab.com:13702/xheart'
 const db = mongoose.createConnection(uristring)
 
 db.once('open', (callback) => {
