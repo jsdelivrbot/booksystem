5e3c8f69af68970e86b401b4c6910ffbd5a4b10d
xheartx <546593797@qq.com>
2017-06-10 00:20:31 +0800

    0

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * @Author: X_Heart
  * @Date: 2017-06-09 10:46:38
  * @Last Modified by: X_Heart
- * @Last Modified time: 2017-06-10 00:14:24
+ * @Last Modified time: 2017-06-10 00:20:00
  * @description: 
  */
 const express = require('express')
@@ -13,6 +13,8 @@ const router = require('./router/router')
 // 设置模板引擎
 app.engine('html', ejs.__express)
 app.set('view engine', 'html')
+
+app.set('port', (process.env.PORT || 5000));
 // 静态文件中间件
 app.use(express.static('./public'))
 // 图书列表页
@@ -26,4 +28,6 @@ app.get('/editbook', router.editBook)
 // 修改图书业务
 app.get('/doedit/:id', router.doEdit)
 
-app.listen(80)
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
