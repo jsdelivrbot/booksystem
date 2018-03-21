59b3f539026bf741e64b73a77defc3485988cd53
X.HEART <546593797@qq.com>
2018-03-21 11:52:30 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@
 const express = require('express')
 const app = express()
 const ejs = require('ejs')
+const bodyParser = require('body-parser')
 const router = require('./router/router')
 const axios = require('axios')
 
